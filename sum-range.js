c483ec3aae86c13607cedb98f3d8de2d732f2f32
Toasty7 <143848640+Toasty7@users.noreply.github.com>
2024-02-02 15:47:30 -0800

    finish onme

--- sum-range.js
+++ sum-range.js
@@ -1,9 +1,21 @@
-function range(start, end, step = 1) {
-  // Add code.
+function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {3+.
+    
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+return array;
 }
 
 function sum(numbers) {
-  // Add code.
+let total = 0
+for (let value of array)
+{
+  total += value;
+}
 }
 
 
@@ -12,7 +24,7 @@ function sum(numbers) {
 
 
 // tests
-console.log(range(1, 10));
+console.log(range(1, 10))
 // → [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
 console.log(range(5, 2, -1));
 // → [5, 4, 3, 2]
