a5372a9b19aea5ca366f8d2457dffc189042bdcf
Toasty7 <143848640+Toasty7@users.noreply.github.com>
2024-02-06 14:38:32 -0800

    all done

--- list.js
+++ list.js
@@ -1,9 +1,20 @@
-function arrayToList(arr) {
-  // Add code.
+function range(start, end, step = start < end ? 1 : -1) {
+  let array = [];
+
+  if (step > 0) {
+    for (let i = start; i <= end; i += step) array.push(i);
+  } else {
+    for (let i = start; i >= end; i += step) array.push(i);
+  }
+  return array;
 }
 
-function listToArray(list, arr) {
-  // Add code.
+function sum(array) {
+  let total = 0;
+  for (let value of array) {
+    total += value;
+  }
+  return total;
 }
 
 // tests
